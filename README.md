b78cb41a85116af03b2562bfeb2a45f0e3a8c836
nikit <kumar.nikit1@gmail.com>
2023-12-19 07:38:19 -0800

    first commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# create conda env
+conda create -p <env_name> python==3.8 -y
+
+# activate env
+conda activate <env_path>
+
+# install packages
+pip install -r requirements.txt
---
+++ app.py
@@ -0,0 +1,46 @@
+from flask import Flask,render_template,request,redirect,url_for
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Hello World"
+
+@app.route('/welcome')
+def welcome():
+    return "Welcome to the flask tutorial"
+
+
+@app.route('/index')
+def index():
+    return render_template('index.html')
+
+@app.route('/success/<int:score>')
+def success(score):
+    return "The person is pass & score is : "+str(score)
+
+@app.route('/fail/<int:score>')
+def fail(score):
+    return "The person is fail & score is : "+str(score)
+
+@app.route('/calculate',methods=['POST','GET'])
+def calculate():
+    if request.method=='GET':
+        return render_template('calculate.html')
+    else:
+        maths=float(request.form['maths'])
+        science=float(request.form['science'])
+        history=float(request.form['history'])
+
+        average_marks=(maths+science+history)/3
+        result="" 
+        if average_marks>=50:
+            result="success"
+        else:
+            result="fail"
+
+        #return redirect(url_for(result,score=average_marks))
+        return render_template('result.html',results=average_marks)
+
+if __name__ == '__main__':
+    app.run(debug = True)
